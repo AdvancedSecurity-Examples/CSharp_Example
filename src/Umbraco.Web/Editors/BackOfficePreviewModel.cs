9bd3267b3aaf0b513fa97dc8ec913be0e499071f
Jack G Kafaty <jackgkafaty@github.com>
2024-12-06 12:42:07 -0600

    C# Code scanning vulnerable app

---
+++ src/Umbraco.Web/Editors/BackOfficePreviewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Umbraco.Core.Configuration;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Umbraco.Web.Composing;
+using Umbraco.Web.Features;
+
+namespace Umbraco.Web.Editors
+{
+    public class BackOfficePreviewModel : BackOfficeModel
+    {
+        private readonly UmbracoFeatures _features;
+        public IEnumerable<ILanguage> Languages { get; }
+
+        [Obsolete("Use the overload that injects IIconService.")]
+        public BackOfficePreviewModel(
+            UmbracoFeatures features,
+            IGlobalSettings globalSettings,
+            IEnumerable<ILanguage> languages)
+            : this(features, globalSettings, languages, Current.IconService)
+        {
+        }
+
+        public BackOfficePreviewModel(
+            UmbracoFeatures features,
+            IGlobalSettings globalSettings,
+            IEnumerable<ILanguage> languages,
+            IIconService iconService)
+            : base(features, globalSettings, iconService)
+        {
+            _features = features;
+            Languages = languages;
+        }
+
+        public bool DisableDevicePreview => _features.Disabled.DisableDevicePreview;
+        public string PreviewExtendedHeaderView => _features.Enabled.PreviewExtendedView;
+    }
+}
